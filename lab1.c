4b274f4b2cf5bf86813f2d6b60c69dc0d06a7f60
SID <99600089+IS1DI@users.noreply.github.com>
2023-02-21 18:04:24 +0300

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -10,19 +10,19 @@ typedef struct attrs{
 } attrs_t;
 
 void* proc(void* args){
-    int *ret = (int*)malloc(sizeof(int));
     attrs_t *arg = (attrs_t*) args;
     printf("Поток %d стартовал\n", arg->id);
+    int *ret = (int*)malloc(sizeof(int));
+    *ret = rand();
     while (arg->is_working)
     {
         printf("Поток %d работает\n", arg->id);
         fflush(stdout);
         sleep(1);
     }
-    *ret = rand();
-    printf("Поток %d завершил работу\n",arg->id);
+    printf("Число rand = %d в %d потоке \nПоток %d завершил работу\n",*ret,arg->id, arg->id);
     pthread_exit(ret);
-};
+};  
 
 
 int main()
@@ -34,7 +34,7 @@ int main()
     args1.is_working = true;
     args2.id = 2;
     args2.is_working = true;
-    int *status1, *status2;
+    int *status1,* status2;
     printf("Основной поток стартовал\n");
     pthread_create(&thread1,NULL,proc, &args1);
     pthread_create(&thread2,NULL,proc, &args2);
@@ -43,11 +43,9 @@ int main()
     args1.is_working= false;
     args2.is_working = false;
     pthread_join(thread1,(void**)&status1);
-    printf("exit code - %p\n",status1);
+    printf("Число rand = %d в main потоке, полученное из %d потока\n",*status1,args1.id);
     pthread_join(thread2,(void**)&status2);
-    printf("exit code - %p\n",status2);
-    free(status1);
-    free(status2);
+    printf("Число rand = %d в main потоке, полученное из %d потока\n",*status2,args2.id);
     printf("Основной поток завершил работу\n");
 
     return 0;
