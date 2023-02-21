caedab8dca48fc17219dbff2d1b5d463b342c30e
SID <99600089+IS1DI@users.noreply.github.com>
2023-02-21 17:01:29 +0000

    lab2

---
+++ lab1/lab1.c
@@ -0,0 +1,52 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "pthread.h"
+#include <unistd.h>
+
+typedef struct attrs{
+    int id;
+    bool is_working;
+} attrs_t;
+
+void* proc(void* args){
+    attrs_t *arg = (attrs_t*) args;
+    printf("Поток %d стартовал\n", arg->id);
+    int *ret = (int*)malloc(sizeof(int));
+    *ret = rand();
+    while (arg->is_working)
+    {
+        printf("Поток %d работает\n", arg->id);
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("Число rand = %d в %d потоке \nПоток %d завершил работу\n",*ret,arg->id, arg->id);
+    pthread_exit(ret);
+};  
+
+
+int main()
+{
+    pthread_t thread1;
+    pthread_t thread2;
+    attrs_t args1,args2;
+    args1.id = 1;
+    args1.is_working = true;
+    args2.id = 2;
+    args2.is_working = true;
+    int *status1,* status2;
+    printf("Основной поток стартовал\n");
+    pthread_create(&thread1,NULL,proc, &args1);
+    pthread_create(&thread2,NULL,proc, &args2);
+    getchar();
+    printf("Клавиша нажата\n");
+    args1.is_working= false;
+    args2.is_working = false;
+    pthread_join(thread1,(void**)&status1);
+    printf("Число rand = %d в main потоке, полученное из %d потока\n",*status1,args1.id);
+    pthread_join(thread2,(void**)&status2);
+    printf("Число rand = %d в main потоке, полученное из %d потока\n",*status2,args2.id);
+    printf("Основной поток завершил работу\n");
+
+    return 0;
+}
