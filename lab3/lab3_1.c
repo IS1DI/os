fbeeabe8623c0a05cb025aa999001e1d3e54077e
SID <99600089+IS1DI@users.noreply.github.com>
2023-03-04 02:56:25 +0300

    Add files via upload

--- lab3/lab3_1.c
+++ lab3/lab3_1.c
@@ -7,23 +7,14 @@
 #include <string.h>
 
 int main(int argc, char **argv){
-    pid_t pid = fork();
-    int exit_code;
-    if (pid == 0){
+    int exit_code = 0;
         printf("message_from_%d_process{\n\tРодительский процесс - %d\n\tДочерний процесс - %d (стартовал)\n}\n", getpid(), getppid(), getpid());
         for (int i = 0; i < argc; i++){
-            printf("%s\n", argv[i]);
+            printf("message_from_%d_process{\n\t%s\n\n}\n",getpid(), argv[i]);
             fflush(stdout);
             sleep(1);
         }
-    }else if (pid > 0){
-        printf("message_from%d_process{\n\tРодительский процесс\n}\n", getpid());
-        fflush(stdout);
-    }else{
-        perror("fork");
-        fflush(stdout);
-    }
+    
     printf("message_from%d_process{\n\tПроцесс %d звершил работу с кодом %d\n}\n", getpid(), getpid(),exit_code);
-    fflush(stdout);
-    return 0;
+    exit(exit_code);
 }
--- lab3/lab3_2.c
+++ lab3/lab3_2.c
@@ -10,30 +10,30 @@
 
 
 int main(int argc, char** argv){
-    //exec() lab3_1.c
-    //ppid pid doch id
-    //args
     int exit_code = 0;
-    printf("message_from_%d_process{\n\tРодительский процесс - %d\n\tДочерний процесс - %d (стартовал)\n}\n", getpid(), getppid(), getpid());
+    
+    printf("message_from_mainX%d_process{\n\tРодительский процесс - %d\n\tДочерний процесс - %d (стартовал)\n}\n", getpid(), getppid(), getpid());
     pid_t pid = fork();
     if(pid==-1){
         perror("fork");
 
         
     }else if(pid==0){
-        char* args[] = {"lab3_1","arg", "ar",NULL};
-        execv("lab3_1",args);
+        char* args[] = {"./lab3_1","arg", "ls",NULL};
+        execv("./lab3_1",args);
         
     }else{
         int code;
-        while(waitpid(pid,&code,WNOHANG) == 0){
-            printf("message_from_%d_process{\n\tЖдём пока процесс %d завершит работу\n}\n",getpid(),pid);
-            sleep(0.5);
+        while(waitpid(pid,&code,WNOHANG) ==0){
+            printf("message_from_mainX%d_process{\n\tЖдём пока процесс %d завершит работу\n}\n",getpid(),pid);
             fflush(stdout);
+            usleep(500000);
+        
+            
         }
-        printf("message_from_%d_process{\n\tПроцесс %d завершил рвботу с кодом %d}\n",getpid(),pid,code);
+        printf("message_from_mainX%d_process{\n\tПроцесс %d завершил рвботу с кодом %d\n}\n",getpid(),pid,code);
     
     }
-    printf("message_from_%d_process{\n\tПроцесс закончил работу c кодом %d\n}\n",getpid(),exit_code);
-    return exit_code;
+    printf("message_from_mainX%d_process{\n\tПроцесс закончил работу c кодом %d\n}\n",getpid(),exit_code);
+    exit(exit_code);
 }
